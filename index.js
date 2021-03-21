8a43f89b07308dbb9fbb955a53b7547844a3525c
Candice Scott-Mason <cscottmason23@gmail.com>
2021-03-21 18:51:26 -0400

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your code here!
+//body.removeChild()
+
+const main = document.getElementById("main");
+main.remove();
+
+const newHeader = document.createElement('h1');
+newHeader.id = "victory";
+
+newHeader.innerHTML = 'Can is the champion';
